f9ba33d9a7b85815a3e5a0346a2fb863bc0e67ea
geanruca <geanruca@gmail.com>
2019-05-09 19:49:07 -0400

    admin panel

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use Illuminate\Http\Request;
 Route::post('login', 'Api\UserController@login');
 Route::post('register', 'Api\UserController@register');
 
-Route::middleware([])
-//Route::middleware(['auth:api'])
+//Route::middleware([])
+Route::middleware(['auth:api'])
 // Route::middleware(['auth'])
 ->namespace('Api')
 ->group(function () {
