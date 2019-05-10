1493d6bf0cea70813132cdc8259aaa840ceb6883
geanruca <geanruca@gmail.com>
2019-05-09 21:40:21 -0400

    admin panel

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -52,6 +52,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware('web')
+             ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
     }
