86f19a683a8fc1fc46b60d587ada8641e5585b00
geanruca <geanruca@gmail.com>
2019-05-09 21:30:08 -0400

    admin panel

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Auth::routes();
 Route::get('/pagoconfirmado',function(){
     return view('flow.exito');
 });
-Route::middleware(['auth:api'])
+Route::middleware(['auth'])
 ->group(function () {
     Route::get('/aguaclean','adminpanel@index');
     Route::get('/aguaclean/usuarios','adminpanel@usuarios');
