81653bb977b8125d9a4050cd220cc2eb0e7db536
geanruca <geanruca@gmail.com>
2020-05-07 14:29:46 -0400

    fixing landing

--- resources/views/mobilechile/home.blade.php
+++ resources/views/mobilechile/home.blade.php
@@ -25,7 +25,9 @@
   </head>
   <body>
       @if((session()->has("flash")))
-        <div class="alert alert-success"><span class="white-text">{{session("flash")}}</span></div>
+        <div class="alert alert-success">
+          <span class="white-text">{{session("flash")}}</span>
+        </div>
       @endif
     
     <div class="phone-wall-intro header white full-height">
@@ -34,7 +36,7 @@
         <div class="col s12 m10 offset-m1 valign-wrapper">
           <div class="valign">
             <h2>Mobile Chile!</h2>
-            <span class="tagline">Vuelve realidad tu App</span>
+            <span class="tagline">Automatización desde tu smarthphone</span>
             <button class="read-more"><i class="icon-caret-down"></i></button>
           </div>
         </div>
@@ -149,7 +151,7 @@
           <div class="row">
             <div class="col s12 m6 l4">
               <h4><i class="small material-icons">money_off </i></h4>
-              <p class="caption">No cobramos por implementación, y si no te sientes conforme con el servicio, simplemente puedes dejar de pagar, sin compromisos.</p>
+              <p class="caption">Vendemos servicios, no productos. No cobramos por implementación, y si no te sientes conforme con el servicio, simplemente puedes dejar de pagar. Sin compromisos.</p>
             </div>
             <div class="col s12 m6 l4">
               <h4><i class="small material-icons">thumb_up </i></h4>
@@ -179,7 +181,7 @@
     </div>
 
     <!-- Pricing Tables -->
-    {{-- <div class="section valign-wrapper full-height">
+    <div class="section valign-wrapper full-height">
         <div class="container">
           <div class="row">
   
@@ -187,22 +189,28 @@
               <div class="pricing-table">
                 <div class="pricing-header">
                   <i class="icon-paper-plane"></i>
-                  <h4>Página para crecer</h4>
+                  <h4>Desarrollador backend senior</h4>
                   <div class="price">
-                    <span class="currency">UF</span>
-                    <span class="dollars">2</span>
-                    <span class="cents">/mes</span>
+                    <span class="currency">USD</span>
+                    <span class="dollars">20</span>
+                    <span class="cents">/hora</span>
                   </div>
                 </div>
                 <ul class="pricing-features">
-                  <li class="pricing-feature"><i class="icon-accept"></i>Página de venta con diseño responsivo</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Mínimo 10 contactos al mes para su negocio</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Integración con redes sociales</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Notificaciones</li>
-                  <li class="pricing-feature disabled"><i class="icon-close"></i>Unificación de todos los medios.</li>
-                  <li class="pricing-feature disabled"><i class="icon-close"></i>Generación de contenido digital.</li>
-                  <li class="pricing-feature disabled"><i class="icon-close"></i>Carrito de venta</li>
-                  <li class="pricing-feature disabled"><i class="icon-close"></i>Desarrollo de sistemas de gestión personalizados</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Control de calidad y buenas prácticas en la programación</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Documentación</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Creación de API</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Sistemas de notificación</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Inteligencia artificial y redes neuronales</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Administración y gestión de emails</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Envíos de email masivos</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Sistemas de autenticación</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Sistemas de administración</li>
+                  <li class="pricing-feature disabled"><i class="icon-accept"></i>Automatización de procesos</li>
+                  <li class="pricing-feature disabled"><i class="icon-accept"></i>Manejo de bases de datos</li>
+                  <li class="pricing-feature disabled"><i class="icon-accept"></i>Testing de integración entre plataformas vía postman</li>
+                  <li class="pricing-feature disabled"><i class="icon-accept"></i>Pagos online</li>
+                  <li class="pricing-feature disabled"><i class="icon-accept"></i>Y un largo etc..</li>
                 </ul>
               </div>
             </div>
@@ -211,56 +219,91 @@
               <div class="pricing-table">
                 <div class="pricing-header">
                   <i class="icon-paper-plane"></i>
-                  <h4>Campaña agresiva</h4>
+                  <h4>Desarrollador Front End</h4>
                   <div class="price">
-                    <span class="currency">UF</span>
-                    <span class="dollars">5</span>
-                    <span class="cents">/mes</span>
+                    <span class="currency">USD</span>
+                    <span class="dollars">20</span>
+                    <span class="cents">/hora</span>
                   </div>
                 </div>
                 <ul class="pricing-features">
-                  <li class="pricing-feature"><i class="icon-accept"></i>Página de venta con diseño responsivo</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Mínimo 30 contactos al mes para su negocio</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Experto en obtención de información e interacción con el usuario</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Experto en integración ágil de contenido variado</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Manejo en diseño responsivo</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Desarrollo de sistemas de gestión personalizados</li>
                   <li class="pricing-feature"><i class="icon-accept"></i>Integración con redes sociales</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Notificaciones</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Unificación de todos los medios</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Generación de contenido digital</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Carrito de venta</li>
-                  <li class="pricing-feature disabled"><i class="icon-close"></i>Desarrollo de sistemas de gestión personalizados</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Visualización de Notificaciones</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Despliegue de elementos interactivos</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Consumo de APIs para obtención de información. </li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Automatización de publicaciones multiplataforma</li>
+                  <li class="pricing-feature"><i class="icon-accept"></i>Lógica de programación orientada a los módulos integrados</li>
+                  <li class="pricing-feature disabled"><i class="icon-close">No es un diseñador</i></li>
                 </ul>
               </div>
             </div>
-  
-            <div class="col s12 m4">
-              <div class="pricing-table">
-                <div class="pricing-header">
-                  <i class="icon-paper-plane"></i>
-                  <h4>Deluxe service</h4>
-                  <div class="price">
-                    <span class="currency">UF</span>
-                    <span class="dollars">10</span>
-                    <span class="cents">/mes</span>
-                  </div>
-                </div>
-                <ul class="pricing-features">
-                  <li class="pricing-feature"><i class="icon-accept"></i>Página de venta con diseño responsivo</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Mínimo 80 contactos al mes para su negocio</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Integración con redes sociales</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Notificaciones</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Unificación de todos los medios.</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Generación de contenido digital.</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Carrito de venta.</li>
-                  <li class="pricing-feature"><i class="icon-accept"></i>Desarrollo de sistemas de gestión personalizados.</li>
-                </ul>
-              </div>
-            </div>
-  
+
   
           </div>
           
         </div>
-      </div> --}}
+      </div>
     
+
+    <!-- Blog -->
+    <div class="section light full-height">
+      <div class="container">
+        <div class="row">
+          <div class="col s12 center">
+            <h1>blog</h1>
+            <div class="row masonry-grid">
+              <div class="col s12 m6 l4">
+                <div class="card">
+                  <div class="card-image">
+                    <a href="blog.html"><img src="{{ URL::to('/images') }}/portafolio6-min.png"></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col s12 m6 l4">
+                <div class="card">
+                  <div class="card-image">
+                    <a href="blog.html"><img src="{{ URL::to('/images') }}/portafolio1-min.png"></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col s12 m6 l4">
+                <div class="card">
+                  <div class="card-image">
+                    <a href="blog.html"><img src="{{ URL::to('/images') }}/portafolio2-min.png"></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col s12 m6 l4">
+                <div class="card">
+                  <div class="card-image">
+                    <a href="blog.html"><img src="{{ URL::to('/images') }}/portafolio3-min.png"></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col s12 m6 l4">
+                <div class="card">
+                  <div class="card-image">
+                    <a href="blog.html"><img src="{{ URL::to('/images') }}/portafolio7-min.png"></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col s12 m6 l4">
+                <div class="card">
+                  <div class="card-image">
+                    <a href="blog.html"><img src="{{ URL::to('/images') }}/portafolio5-min.jpg"></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <!-- Contact Us -->
     
     <div class="section light valign-wrapper" id="#cafe">
